952a0071d77301606ba58672605e0888acc93f08
Mdparvez786 <mdp09906@gmail.com>
2024-07-23 17:11:52 +0530

    java program

---
+++ CheckIntegerLiteral.java
@@ -0,0 +1,14 @@
+public class CheckIntegerLiteral{
+ public static void main(String[] args){
+    //int a=10;
+	 //int b=010;
+	 //int c=0x10;
+	 //System.out.println(a+".."+b+".."+c);
+	 int a=10;
+	 int b=20;
+	 int c=024;
+	 System.out.println(a+b+c);
+ }
+
+
+}
